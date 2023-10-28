45e6fa23c29562e87e7a7c985c0804ca77e1e443
Vitals9367 <vitalijus.alsauskas@gmail.com>
2023-10-28 19:07:07 +0300

    output directory

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/travel-app',
+    assetPrefix: '/travel-app/', // assetPrefix requires the trailing slash
     distDir: 'out',
     images: {
         unoptimized: true
